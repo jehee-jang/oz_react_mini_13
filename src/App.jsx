e2a7738ebf085b2ba6e79d8d0c92785a6e291579
jehee-jang <jehee010@naver.com>
2025-09-03 23:40:52 +0900

    feat: 스와이퍼 이용한 슬라이드 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import MovieCard from "./component/MovieCard";
 import movieListData from "./assets/data/movieListData.json"
 import { Routes, Route } from "react-router-dom";
@@ -6,24 +5,36 @@ import MovieDetail from "./component/MovieDetail";
 import './App.css'
 import Layout from "./component/Layout";
 
+import 'swiper/css';
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
 
 function List() {
-  const [movies] = useState(movieListData.results);
-
+  
   return (
     <div className="app">
       <h1>영화 목록</h1>
-        <div className="movie-list">
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              id={movie.id}
-              title={movie.title}
-              voteaverage={movie.vote_average}
-              posterUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            />
-          ))}
-        </div>
+    <Swiper
+    modules={[Navigation]}// 사용할 모듈
+    spaceBetween={16}// 슬라이드 사이 간격
+    slidesPerView={5}// 슬라이드 한 화면에 몇개 보여줄건지
+    navigation // 좌우 버튼 활성화
+  >
+    {movieListData.results.map((movie) => (
+        <SwiperSlide key={movie.id}>
+          <MovieCard
+            id={movie.id}
+            title={movie.title}
+            posterUrl={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            voteaverage={movie.vote_average}
+          />
+        </SwiperSlide>
+      ))}
+  </Swiper>
+      
+        
     </div>
   );
 }
