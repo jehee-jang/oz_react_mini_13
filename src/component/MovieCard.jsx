61074a0acaebeef9c25e1bd158df8abef8b84eca
jehee-jang <jehee010@naver.com>
2025-09-09 11:01:31 +0900

    feat:3단계 구현 완료

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -1,14 +1,19 @@
 import { useNavigate } from "react-router-dom";
 
-function MovieCard({ id, title, voteaverage, posterUrl }) {
+function MovieCard({ id, title, voteaverage, backdropUrl }) {
     const navigate = useNavigate()
     return (
-    <div onClick={() => navigate(`/detail/${id}`)} className="movie-card">
-        <img src={posterUrl} alt={title} />
-        <h2>{title}</h2>
-        <p>{voteaverage}</p>
+    <div className="flex justify-center min-h-screen px-4">
+    <div onClick={() => navigate(`/detail/${id}`)} className=" p-2.5 text-center overflow-hidden w-full sm:w-[80%] md:w-[70%] lg:w-[60%] h-auto">
+        <img className="rounded-lg w-full block aspect-video object-cover" src={backdropUrl} alt={title} />
+        <h2 className="text-lg sm:text-xl md:text-2xl mt-2 mb-1 font-bold">{title}</h2>
+        <p className="text-base sm:text-lg md:text-xl mb-2">{voteaverage}</p>
+    </div>
     </div>
     );
 }
 
 export default MovieCard;
+
+// aspect-video -> 16:9 비율 유지
+//  object-cover -> 넘치는 부분 자름
